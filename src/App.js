6c60e5075fb7e6dbd014cd07d4bee4747521fd75
Sean Sipus <sean.sipus@gmail.com>
2022-12-20 02:46:47 -0500

    Add styling to cards

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import ActivityFilter from './components/MembersCards';
 function App() {
   return (
     <div className="App">
+      <div className="wrapper">
       <ActivityFilter />
+      </div>
     </div>
   );
 }
--- src/components/MembersCards.jsx
+++ src/components/MembersCards.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "../App.css";
 
 const DUMMY_MEMBERS = [
   {
@@ -7,7 +8,7 @@ const DUMMY_MEMBERS = [
     name: "John",
     age: 10,
     rating: 4.5,
-    activities: ["hiking", "swimming", "biking"]
+    activities: ["hiking", "swimming", "biking"],
   },
 
   {
@@ -16,7 +17,7 @@ const DUMMY_MEMBERS = [
     name: "Paul",
     age: 12,
     rating: 5,
-    activities: ["swimming", "crafts", "plant identification"]
+    activities: ["swimming", "crafts", "plant identification"],
   },
 
   {
@@ -25,7 +26,7 @@ const DUMMY_MEMBERS = [
     name: "George",
     age: 11,
     rating: 3.2,
-    activities: ["volleyball", "tennis", "rowing"]
+    activities: ["volleyball", "tennis", "rowing"],
   },
 
   {
@@ -34,50 +35,79 @@ const DUMMY_MEMBERS = [
     name: "Ringo",
     age: 13,
     rating: 5,
-    activities: ["fishing", "tennis", "tree climbing"]
+    activities: ["fishing", "tennis", "tree climbing"],
   },
- 
+
+  {
+    img: "https://placekitten.com/201/201",
+    id: "a5",
+    name: "Alex",
+    age: 6,
+    rating: 2.8,
+    activities: ["fishing", "crafts", "plant identification"],
+  },
+
+  {
+    img: "https://placekitten.com/202/202",
+    id: "a6",
+    name: "Andrew",
+    age: 8,
+    rating: 4.4,
+    activities: ["volleyball", "tennis", "swimming"],
+  },
 ];
 
 const MemberCards = ({ members }) => {
   return (
-    <div>
-      {members.map(member => (
-        <div key={member.id}>
-          <img src={member.img} alt={member.name} />
-          <h3>{member.name}</h3>
-          <p>Age: {member.age}</p>
-          <p>Rating: {member.rating}</p>
-          <p>Activities: {member.activities.join(', ')}</p>
-        </div>
-      ))}
-    </div>
+      <div className="memberCardContainer">
+        {members.map((member) => (
+          <div key={member.id} className="memberCard">
+            <img src={member.img} alt={member.name} />
+            <h3>{member.name}</h3>
+            <p>Age: {member.age}</p>
+            <p>Rating: {member.rating}</p>
+            <p>Activities: {member.activities.join(", ")}</p>
+          </div>
+        ))}
+      </div>
   );
 };
 
 const ActivityFilter = () => {
   const [filteredMembers, setFilteredMembers] = useState(DUMMY_MEMBERS);
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     const selectedActivity = event.target.value;
     setFilteredMembers(
-      selectedActivity === ''
+      selectedActivity === ""
         ? DUMMY_MEMBERS
-        : DUMMY_MEMBERS.filter(member => member.activities.includes(selectedActivity))
+        : DUMMY_MEMBERS.filter((member) =>
+            member.activities.includes(selectedActivity)
+          )
     );
   };
 
   return (
     <div>
-      <label htmlFor="activity-select">Select an activity:</label>
-      <select id="activity-select" onChange={handleChange}>
-        <option value="">All</option>
-        {Array.from(new Set(DUMMY_MEMBERS.flatMap(member => member.activities))).sort().map(act => (
-          <option key={act} value={act}>
-            {act}
-          </option>
-        ))}
-      </select>
+      <div className="activitySelector">
+        <label htmlFor="activity-select">Select an activity:</label>
+        <select
+          id="activity-select"
+          onChange={handleChange}
+          className="customSelect"
+        >
+          <option value="">All</option>
+          {Array.from(
+            new Set(DUMMY_MEMBERS.flatMap((member) => member.activities))
+          )
+            .sort()
+            .map((act) => (
+              <option key={act} value={act}>
+                {act}
+              </option>
+            ))}
+        </select>
+      </div>
       <MemberCards members={filteredMembers} />
     </div>
   );
